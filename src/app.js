f91ed51fd8feffd29e91490bcb139b575e5bb733
Poltier <poltr96@gmail.com>
2022-12-21 23:07:07 +0100

    penultimo dia

--- src/app.js
+++ src/app.js
@@ -5,16 +5,3 @@ window.addEventListener('load', vanish);
 function vanish() {
   loader.classList.add('disppear');
 }
-
-// Obtener el botón y el menú
-var button = document.querySelector('.menu-button');
-var menu = document.querySelector('.menu');
-
-// Mostrar y ocultar el menú al hacer clic en el botón
-button.addEventListener('click', function () {
-  if (menu.style.display === 'block') {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'block';
-  }
-});
